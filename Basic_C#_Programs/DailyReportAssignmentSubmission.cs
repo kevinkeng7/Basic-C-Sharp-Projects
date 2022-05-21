303859af8d9203bbc9e15e3b4f177d5dd1ee1ddf
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-05-21 00:47:10 -0700

    Add files via upload

--- Basic_C#_Programs/DailyReportAssignmentSubmission.cs
+++ Basic_C#_Programs/DailyReportAssignmentSubmission.cs
@@ -4,32 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace VariablesAndDataTypes
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //This is a simple program
-            Console.WriteLine("Academy of Learning Career College");
-            Console.WriteLine("Student Daily Report");
-            Console.WriteLine("What course are you in?");
-            string courseName = Console.ReadLine();
-            Console.WriteLine("Your course is: " + courseName);
-            Console.ReadLine();
+            //Console.WriteLine("Academy of Learning Career College");
+            //Console.WriteLine("Student Daily Report");
+            //Console.WriteLine("What course are you in?");
+            //string courseName = Console.ReadLine();
+            //Console.WriteLine("Your course is: " + courseName);
+            //Console.ReadLine();
 
-            Console.WriteLine("What page number?");
-            string pageNumber = Console.ReadLine();
-            Console.WriteLine("Your page number is: " + pageNumber);
-            Console.ReadLine();
+            //Console.WriteLine("What page number?");
+            //string pageNumber = Console.ReadLine();
+            //Console.WriteLine("Your page number is: " + pageNumber);
+            //Console.ReadLine();
 
-            Console.WriteLine("Do you need help with anything? Please answer true or false");
-            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
-            Console.WriteLine("How many hours did you study today?");
-            Console.ReadLine();
+            //Console.WriteLine("Do you need help with anything? Please answer true or false");
+            //Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            //Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            //Console.WriteLine("How many hours did you study today?");
+            //Console.ReadLine();
 
-            Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+            //Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
 
 
             //Console.WriteLine("What is your favorite number?");
@@ -42,7 +43,7 @@ namespace VariablesAndDataTypes
             //bool isStudying = false;
             //byte hoursWorked = 42;
             //sbyte currentTemperature = -23;
-            
+
             //char questionMark = '\u2103';
 
             //decimal moneyInBank = 100.5m;
@@ -55,9 +56,63 @@ namespace VariablesAndDataTypes
 
             //string myName = "Jesse";
 
-            bool isRaining = true;
-            string rainingStatus = Convert.ToString(isRaining);
-            Console.WriteLine(rainingStatus);
+            //bool isRaining = true;
+            //string rainingStatus = Convert.ToString(isRaining);
+            //Console.WriteLine(rainingStatus);
+            //Console.ReadLine();
+
+
+            //int num1 = 100;
+            //int num2 = 10;
+            //int total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //int num1 = 100;
+            //int num2 = 10;
+            //int difference = num1 - num2;
+            //Console.WriteLine(difference);
+            //Console.ReadLine();
+
+            //int num1 = 100;
+            //int num2 = 10;
+            //int product = num1 * num2;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int num1 = 100;
+            //int num2 = 10;
+            //int quotient = 100 / 10;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //double num1 = 7.03230923;
+            //float num2 = 5.9f;
+            //double total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //string firstName = "Jesse \n";
+            //string lastName = "Johnson";
+            //Console.WriteLine(firstName + lastName);
+            //Console.ReadLine();
+
+            //decimal num1 = 8.9563m;
+            //sbyte num2 = 9;
+            //decimal total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //double num1 = 16.43230923;
+            //float num2 = 8.8f;
+            //float total = (float)num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            decimal num1 = 19.75488m;
+            int num2 = 6;
+            decimal total = num1 + num2;
+            Console.WriteLine(total);
             Console.ReadLine();
 
         }
