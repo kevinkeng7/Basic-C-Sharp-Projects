8c14fad1ef85079d3d551821077eeb1fa3f49ebf
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-05-22 01:13:35 -0700

    Add files via upload

---
+++ Basic_C#_Programs/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string userAge = Console.ReadLine();
+            int ageNum = Convert.ToInt32(userAge);
+
+            Console.WriteLine("Have you ever had a DUI? Answer true or false.");
+            string userDui = Console.ReadLine();
+
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string ticket = Console.ReadLine();
+            int ticketNum = Convert.ToInt32(ticket);
+
+            bool ageQualify = ageNum > 15;
+            bool duiQualify = userDui == "false";
+            bool ticketQualify = ticketNum < 3;
+
+            Console.WriteLine("Qualified?");
+            bool resultQualify = (ageQualify && duiQualify && ticketQualify);
+            Console.WriteLine(resultQualify);
+
+            Console.ReadLine();
+           
+            //int age = 31;
+            //bool hasTicket = true;
+            //bool hasPassport = true;
+            //bool canFly = (age >= 18 && hasTicket && hasPassport);
+            //Console.WriteLine(canFly);
+            //Console.ReadLine();
+
+
+        }
+    }
+}
--- Basic_C#_Programs/MathAndComparisonOperators.Program/MathAndComparisonOperators.Program/Program.cs
+++ Basic_C#_Programs/MathAndComparisonOperators.Program/MathAndComparisonOperators.Program/Program.cs
@@ -37,18 +37,64 @@ namespace MathAndComparisonOperators.Program
             //Console.Write(trueOrFalse.ToString());
             //Console.ReadLine();
 
-            int roomTemperature = 70;
-            int currentTemperature = 100;
+            //int roomTemperature = 70;
+            //int currentTemperature = 100;
 
             //bool isWarm = currentTemperature >= roomTemperature;
             //bool isWarm = currentTemperature <= roomTemperature;
-            bool isWarm = currentTemperature != roomTemperature;
+            //bool isWarm = currentTemperature != roomTemperature;
             //bool isWarm = currentTemperature == roomTemperature;
 
-            Console.WriteLine(isWarm);
-            Console.ReadLine();
-         
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
 
+            //int num1 = 15;
+            //int num2 = 8;
+            //int remainder = num1 % num2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //int packageWidth = 10;
+            //int packageHeight = 25;
+            //bool tooBig = (packageHeight > 25 || packageWidth > 25);
+
+            //Console.WriteLine("True or false: Are either the package height or the package width greater than 25?");
+            //Console.WriteLine(tooBig);
+            //Console.ReadLine();
+
+            //int age = 31;
+            //bool hasTicket = true;
+            //bool hasPassport = true;
+            //bool canFly = (age >= 18 && hasTicket && hasPassport);
+            //Console.WriteLine(canFly);
+            //Console.ReadLine();
+
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            //Console.WriteLine(true && true && true && true && false);
+            //Console.WriteLine(true && true && true && true || false);
+            //Console.WriteLine(true || false && true || false);
+            //Console.WriteLine(true || false && true && false);
+            Console.WriteLine(true && true && true && false);
+            Console.ReadLine();
         }
     }
 }
