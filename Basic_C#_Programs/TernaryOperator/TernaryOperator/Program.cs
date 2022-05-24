f2f96eff794ee23c0891d3610f177e6ed002eeeb
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-05-23 21:12:19 -0700

    Add files via upload

---
+++ Basic_C#_Programs/TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.")
+            }
+
+            Console.ReadLine();
+
+            //If else, else if, Ternary operator examples below:
+
+            //Console.WriteLine("What is your favorite number?");
+            //int favNum = Convert.ToInt32(Console.ReadLine());
+
+            //string result = favNum == 12 ? "You have a cool fav num" : "You do not have a cool fav num";
+
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hi, What is your name? ");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature where you are.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature where you are.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature where you are.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Uhhhh.... something went wrong.");
+            //}
+            //Console.ReadLine();
+
+            //int num1 = 100;
+            //int num2 = 12;
+
+            //string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+            //int currentTemperature = 70;
+            //int roomTemperature = 70;
+
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is room temp." : "It is not room temp.";
+            //Console.WriteLine(comparisonResult);
+            //Console.ReadLine();
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (currentTemperature < roomTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature.");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
