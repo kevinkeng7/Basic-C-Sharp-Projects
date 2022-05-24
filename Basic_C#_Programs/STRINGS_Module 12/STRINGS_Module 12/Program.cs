e8003c930c420f63b72f915d0b0634ab4f54563f
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-05-23 23:22:06 -0700

    Add files via upload

---
+++ Basic_C#_Programs/STRINGS_Module 12/STRINGS_Module 12/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STRINGS_Module_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jesse";
+            //string quote = "The man said, \\Hello\", Jesse. \n Hello on a new line. \n \t Hello on a tab.";
+            //string fileName = "C:\\Users\\Jesse";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            string name = "jesse";
+            name = name.ToUpper();
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+            string name = "jesse";
+            name = name.ToLower();
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            //string userName = "<Type your name here>";
+            //string dateString = DateTime.Today.ToShortDateString();
+
+            //// Use the + and += operators for one-time concatenations.
+            //string str = "Hello " + userName + ". Today is " + dateString + ".";
+            //Console.WriteLine(str);
+            //Console.ReadLine();
+
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append("My name is Jesse");
+            //Console.WriteLine(sb);
+            //Console.ReadLine();
+
+
+        }
+    }
+}
