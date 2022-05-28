ccc65070138e10cc2dd0e1abf043f2b71d87cf3c
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-05-27 23:19:51 -0700

    Add files via upload

---
+++ Basic_C#_Programs/Sandbox ART/Sandbox ART/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox_ART
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool result = (true && (true || false));
+            bool result = (true ^ true) || (true ^ false);
+
+            //int num1 = 43;
+            //int num2 = 37;
+
+            //string result = num1 > num2 ? "num1 is greater than num2" : "num1 is less than num2";
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
