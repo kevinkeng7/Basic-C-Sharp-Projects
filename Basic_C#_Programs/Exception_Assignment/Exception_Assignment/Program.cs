a815690ab6420b6fd06896a6d04ac0d732243652
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-05-28 21:38:58 -0700

    Add files via upload

---
+++ Basic_C#_Programs/Exception_Assignment/Exception_Assignment/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list of integers. Ask the user for a number to divide each number in the list by. Write a loop that takes each integer in the list, divides it by the number the user entered, and displays the result to the screen.
+
+            Console.WriteLine("List of numbers: 100, 200, 300, 400, 500");
+            List<int> userNum = new List<int> { 100, 200, 300, 400, 500 };
+
+            try
+            {
+                Console.WriteLine("Please enter a number to divide each number in the list by.");
+                userNum.ForEach(Console.WriteLine);
+                int divisor = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine("Calculating... Press 'enter' for next number");
+
+                foreach (int number in userNum)
+                {
+                    int quotient = number / divisor;
+                    Console.WriteLine(quotient);
+                    Console.ReadLine();
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program terminated. Goodbye");
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+
+           
+
+        }
+    }
+}
