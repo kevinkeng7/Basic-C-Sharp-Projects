14d20d05ac6adcf21dcdf8ecc329d852f7ef6d0f
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-05-30 01:39:41 -0700

    Add files via upload

---
+++ Basic_C#_Programs/TwentyOne_or_BlackJack/TwentyOne_or_BlackJack/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne_or_BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            deck = Shuffle(deck);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> TempList = new List<Card>();
+            Random random = new Random();
+            
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                TempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+            deck.Cards = TempList;
+            return deck;
+        }
+    }
+}
+
+//deck.Cards = new List<Card>();
+
+//Card cardOne = new Card();
+//cardOne.Face = "Queen";
+//cardOne.Suit = "Spades";
+
+//deck.Cards.Add(cardOne);
+
+//Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
+//Console.ReadLine();
+
+//Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit);
+//Console.ReadLine();
