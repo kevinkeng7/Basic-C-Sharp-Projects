fde2e2721717bd5804a56d1c59e893a40a654851
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-06-06 01:13:38 -0700

    Add files via upload

---
+++ Basic_C#_Programs/Operator_Overloading_Assignment/Operator_Overloading_Assignment/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading_Assignment
+{
+    public class Employee
+    {
+        public static bool operator ==(Employee a, Employee b)
+        {
+            return Equals(a.Id, b.Id); //Id
+        }
+
+        public static bool operator !=(Employee a, Employee b)
+        {
+            return !Equals(a.Id, b.Id);
+        }
+
+        public int Id { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/Operator_Overloading_Assignment/Operator_Overloading_Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Jesse = new Employee(); //Employee 1
+            Jesse.Id = 007;
+            Employee James = new Employee(); //Employee 2
+            James.Id = 007;
+            bool compare = (Jesse == James);
+            if (compare)        //compare Id property of Employee 1 and Employee 2
+            {
+                Console.WriteLine("Employee IDs match"); //if match
+            }
+            else
+            {
+                Console.WriteLine("Employee IDs do not match"); //if do not match
+            }
+            Console.ReadLine();
+        }
+    }
+}
