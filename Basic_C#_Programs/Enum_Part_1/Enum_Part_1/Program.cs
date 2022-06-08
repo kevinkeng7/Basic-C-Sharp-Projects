190f8d0b70b134560049c6e7fa61e7636ad7f0be
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-06-08 03:09:08 -0700

    Add files via upload

---
+++ Basic_C#_Programs/Enum_Part_1/Enum_Part_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysOfTheWeek day = DaysOfTheWeek.Saturday;
+            ConsoleColor color = ConsoleColor.Green;
+
+            public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+    }
+}
