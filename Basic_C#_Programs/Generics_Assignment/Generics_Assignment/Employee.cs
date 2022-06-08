706f32222732d2125f92e40cd7a7dd9caf160e58
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-06-08 00:24:05 -0700

    Add files via upload

---
+++ Basic_C#_Programs/Generics_Assignment/Generics_Assignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    public class Employee<T> //Make the Employee class take a generic type parameter
+    {
+        public List<T>Things { get; set; } //Add a property to the Employee class called "things" and have its data type be a generic list matching the generic type of the class
+    }
+}
---
+++ Basic_C#_Programs/Generics_Assignment/Generics_Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>(); //Instantiate an Employee object with type "string" as its generic parameter
+            employee.Things = new List<string>() { "Thing_1", "Thing_2", "Thing_3", "Thing_4", "Thing_5" }; //Assign a list of strings as the property value of Things.
+
+            Employee<int> employeeNumber = new Employee<int>(); //Instantiate an Employee object with type "int" as its generic parameter
+            employeeNumber.Things = new List<int>() { 5, 10, 15, 20, 25 }; //Assign a list of integers as the property value of Things.
+
+            foreach (string thing in employee.Things) //Create a loop that prints all of the Things to the Console.
+            {
+                Console.WriteLine(thing);
+                Console.ReadLine();
+            }
+
+            foreach (int thing in employeeNumber.Things) //Create a loop that prints all of the Things to the Console.
+            {
+                Console.WriteLine(thing);
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
