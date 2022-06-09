4cf48eca243d4f7dba1cf6b4b99bd1443dc1a7e8
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-06-09 00:41:26 -0700

    Add files via upload

---
+++ Basic_C#_Programs/Structs_Assignment/Structs_Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs_Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Number number = new Number(); //In the Main() method, create an object of data type Number
+            number.Amount = 777; //assign an amount to it.
+            Console.WriteLine(number.Amount); //Print this amount to the console.
+            Console.ReadLine(); 
+
+        }
+
+        public struct Number //Create a struct called Number
+        {
+            public decimal Amount { get; set; } //give it the property "Amount" and have it be of data type decimal.
+            
+        }
+        
+    }
+}
