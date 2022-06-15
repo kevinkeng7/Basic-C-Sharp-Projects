48900046717cd2fbf1ba5cb1ee3fc6d90d135ac4
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-06-15 03:13:07 -0700

    Add files via upload

---
+++ Basic_C#_Programs/DateTime_Assignment/DateTime_Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now; //Prints the current date and time to the console.
+            Console.WriteLine(dateTime);
+
+            Console.WriteLine("Input a number. This will be # of hours added to current time"); //Asks the user for a number.
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            DateTime dateTime1 = dateTime.AddHours(+X); //Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+            Console.WriteLine(dateTime1);
+            Console.Read();
+
+        }
+    }
+}
