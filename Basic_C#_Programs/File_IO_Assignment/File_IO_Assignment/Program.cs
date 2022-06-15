980db61dc260fd70962b9a3cc0458b3efcf32e53
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-06-15 00:45:19 -0700

    Add files via upload

---
+++ Basic_C#_Programs/File_IO_Assignment/File_IO_Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number."); //Asks the user for a number.
+            string number = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\kevinkeng\Logs\number.txt", number);  //Logs that number to a text file.
+
+            string read_number = File.ReadAllText(@"C:\Users\kevinkeng\Logs\number.txt");  //Prints the text file back to the user.
+            Console.WriteLine(read_number);
+            Console.Read();
+        }
+    }
+}
