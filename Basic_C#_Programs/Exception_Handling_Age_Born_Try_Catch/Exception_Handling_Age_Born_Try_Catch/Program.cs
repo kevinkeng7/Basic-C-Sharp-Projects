36d7d455aeb42a6f4f19402650e0628ea85a0e76
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-06-18 01:14:09 -0700

    Add files via upload

---
+++ Basic_C#_Programs/Exception_Handling_Age_Born_Try_Catch/Exception_Handling_Age_Born_Try_Catch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_Age_Born_Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What year were you born?"); //Display the year the user was born.
+                int yearBorn = Convert.ToInt32(Console.ReadLine());
+                if (yearBorn <= 0) // check if user enters zero or negative numbers
+                {
+                    throw new ArgumentException();
+                }
+                Console.WriteLine("You were born in: {0}", yearBorn); //display year user born
+                var today = DateTime.Today;
+                var age = today.Year - yearBorn;
+                Console.WriteLine("Your age is: {0}", age);
+                Console.ReadLine();
+            }
+            catch (ArgumentException) //Display error message if user enters zero or negative numbers
+            {
+                Console.WriteLine("Please enter a number greater than 0 in a valid year format.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception) //Display general error message if exception was caused by anything else
+            {
+                Console.WriteLine("An error has occurred. Please contact your System Admin.");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
